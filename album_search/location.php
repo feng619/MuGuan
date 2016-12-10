92c0a6990de62bfd2b05a0bc8893847a50a0b255
Feng <feng619@outlook.com>
2016-12-10 16:04:21 +0800

    update1210-1
    update1210-1

---
+++ album_search/location.php
@@ -0,0 +1,31 @@
+
+
+<?php
+echo "
+				<div class='s34_2box'>
+					<p class='album_name'>台北福州公園</p>
+					<a href='#' class='imgbox'><img src='https://scontent-tpe1-1.xx.fbcdn.net/t31.0-8/11838701_1114394921934462_8034811869475435580_o.jpg'></a>
+				</div>
+				<div class='s34_2box'>
+					<p class='album_name'>新竹觀霧</p>
+					<a href='#' class='imgbox'><img src='https://scontent-tpe1-1.xx.fbcdn.net/t31.0-8/12038986_1114198751954079_2426156702801118768_o.jpg'></a>
+				</div>
+				<div class='s34_2box'>
+					<p class='album_name'>二萬平阿里山</p>
+					<a href='#' class='imgbox'><img src='https://scontent-tpe1-1.xx.fbcdn.net/t31.0-8/12028661_1114205281953426_528850639178027132_o.jpg'></a>
+				</div>
+				<div class='s34_2box'>
+					<p class='album_name'>阿里山青年活動中心</p>
+					<a href='#' class='imgbox'><img src='https://scontent-tpe1-1.xx.fbcdn.net/t31.0-8/12017670_1114391995268088_8260121806133470954_o.jpg'></a>
+				</div>
+				<div class='s34_2box'>
+					<p class='album_name'>高雄藤枝部落</p>
+					<a href='#' class='imgbox'><img src='https://scontent-tpe1-1.xx.fbcdn.net/t31.0-8/12034491_1114198888620732_6475287739809919252_o.jpg'></a>
+				</div>
+				<div class='s34_2box'>
+					<p class='album_name'>何季耕蛾類觀察記錄</p>
+					<a href='#' class='imgbox'><img src='https://scontent-tpe1-1.xx.fbcdn.net/t31.0-8/12032856_1114406318599989_5395671498528550326_o.jpg'></a>
+				</div>
+
+";
+?>
